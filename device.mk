7db58a96a0c7e84b0a7b5030827f5d442e82a9e2
Савва Менский <az09209@yandex.ru>
2021-04-28 23:58:21 +0700

    fix typo

--- device.mk
+++ device.mk
@@ -154,7 +154,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.surya \ 
+    android.hardware.power-service.surya \
     init.safailnet.rc
 
 # Product init
